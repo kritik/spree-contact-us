4f8dd3a837d503c7901ba3edc888eb80585e4195
Travis <numbers1311407@gmail.com>
2010-04-25 15:05:07 -0400

    changed mailer to pass recipients as an array

--- app/models/inquiry_mailer.rb
+++ app/models/inquiry_mailer.rb
@@ -2,8 +2,8 @@ class InquiryMailer < ActionMailer::Base
   def notification(inquiry)
     subject    "#{Spree::Config[:site_name]} Contact Us Form Received" rescue "Contact Us Form Received"
     body       :inquiry => inquiry
-    recipients Spree::Config[:contact_form_to]  rescue ''
-    bcc        Spree::Config[:contact_form_bcc] rescue ''
+    recipients [Spree::Config[:contact_form_to]]  rescue ['']
+    bcc        [Spree::Config[:contact_form_bcc]] rescue ['']
     from       inquiry.email
   end
 end
