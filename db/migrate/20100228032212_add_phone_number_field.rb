406911f506b8b15d22e4da54d9247327bb17828c
sbeam <sbeam@onsetcorps.net>
2010-11-18 23:27:06 -0500

    woops, dont need to add phone number twice

--- db/migrate/20100228032212_add_phone_number_field.rb
+++ db/migrate/20100228032212_add_phone_number_field.rb
@@ -1,6 +1,6 @@
 class AddPhoneNumberField < ActiveRecord::Migration
   def self.up
-    add_column :inquiries, :phone_number, :string
+    add_column :inquiries, :phone_number_alt, :string
   end
 
   def self.down
